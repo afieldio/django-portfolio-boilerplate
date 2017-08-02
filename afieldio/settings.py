36051b61ed9b4c74c3cd8603369626cb89684e0f
afieldio <a.field738@gmail.com>
2017-08-02 21:14:15 +0100

    refactored roodle js

--- afieldio/settings.py
+++ afieldio/settings.py
@@ -121,7 +121,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-GOOGLE_RECAPTCHA_SECRET_KEY = 'secret-password'
+GOOGLE_RECAPTCHA_SECRET_KEY = '6LdvSSkUAAAAABZYSY_WgM6JwRMGmFL-MuXrhG80'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
