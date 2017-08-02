af3818be3c3dd6c17b5fa0df476be5057bf49f16
afieldio <a.field738@gmail.com>
2017-08-02 09:01:47 +0100

    content updates

--- portfolio/static/portfolio/js/header.js
+++ portfolio/static/portfolio/js/header.js
@@ -8,8 +8,8 @@
     addListeners();
 
     function initHeader() {
-        width = window.outerWidth;
-        height = window.outerHeight;
+        width = window.innerWidth;
+        height = window.innerHeight;
         target = {x: width/2, y: height/2};
 
         largeHeader = document.getElementById('large-header');
